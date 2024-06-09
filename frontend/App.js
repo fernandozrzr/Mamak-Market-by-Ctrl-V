95ea3f9117a7030326c2315cabdc0f81a5713d6e
IvanC1234 <127371847+IvanC1234@users.noreply.github.com>
2024-06-09 23:03:50 +0800

    Update App.js

--- frontend/App.js
+++ frontend/App.js
@@ -110,7 +110,7 @@ function SellerScreens() {
 
         },
         headerShown: false,
-      }} component={SellerListing} />     
+      }} component={AddLiSTING} />     
       <Tab.Screen name="Feed" options={{
         tabBarStyle: {
           paddingBottom: 8,
