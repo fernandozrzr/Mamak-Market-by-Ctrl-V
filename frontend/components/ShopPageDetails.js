e9009db042a3e65f61133767f142dbfff9b3b5ca
Fernando <fernandozrzr@gmail.com>
2024-06-13 00:20:31 +0800

    dynamic pricing model implement

--- frontend/components/ShopPageDetails.js
+++ frontend/components/ShopPageDetails.js
@@ -1,7 +1,48 @@
 import React from 'react';
 import { Text, TouchableOpacity, Image, View } from 'react-native';
 
+import { useState } from 'react';
+
 const ShopPageDetails = ({ item, navigation }) => {
+
+    const [condtion, setCondition] = useState(false);
+    const daysPosted = diffInDays(new Date(item.createdAt), new Date(item.expirydate));
+    const daysRemaining = diffInDays(new Date(), new Date(item.expirydate));
+
+    function diffInDays(lowerdate, upperdate) {
+        const lower = new Date(lowerdate);
+        const upper = new Date(upperdate);
+
+        const formattedLower = new Intl.DateTimeFormat('en-UK', {
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric'
+        }).format(lower);
+        const formattedUpper = new Intl.DateTimeFormat('en-UK', {
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric'
+        }).format(upper);
+
+        const days = Math.round((upper - lower) / (1000 * 60 * 60 * 24))
+        return days;
+    }
+
+    function dynamicPricing(item, setCondition) {
+        const currentDate = new Date(); // Current date
+        const expiryDate = new Date(item.expirydate); // Expiry date
+        const datePosted = new Date(item.createdAt); // Date item was posted
+
+        // Format dates for display
+
+        const daysPosted = diffInDays(datePosted, expiryDate);
+        const discount = item.cost / daysPosted;
+        const remainingDays = diffInDays(currentDate, expiryDate);
+
+        const finalPrice = discount * remainingDays;
+        return finalPrice.toFixed(2);
+    }
+
     return (
         <TouchableOpacity
             style={{
@@ -26,8 +67,8 @@ const ShopPageDetails = ({ item, navigation }) => {
                 }}
             >{item.item}</Text>
             <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-                <Text style={{ textDecorationLine: 'line-through', fontSize: 15 }}>${item.cost}</Text>
-                <Text style={{ marginLeft: 5, fontSize: 15, color: 'green' }}>${(item.cost * 0.7)}</Text>
+                {((daysPosted !== daysRemaining) || (daysRemaining > daysPosted)) && <Text style={{ textDecorationLine: 'line-through', fontSize: 15 }}>${item.cost}</Text>}
+                <Text style={{ marginLeft: 5, fontSize: 15, color: 'green' }}>${dynamicPricing(item, setCondition)}</Text>
             </View>
         </TouchableOpacity>
     );
--- frontend/config.js
+++ frontend/config.js
@@ -1,6 +1,10 @@
 // config.js
 const config = {
+<<<<<<< Updated upstream
     API_URL: 'http://192.168.1.12:4000/api',
+=======
+    API_URL: 'http://192.168.18.17:4000/api',
+>>>>>>> Stashed changes
 };
 
 export default config;
